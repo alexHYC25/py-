8888173fdd2872667675f81b5c4072ef4e62f8c6
junyan0911 <153891266+junyan0911@users.noreply.github.com>
2023-12-25 19:27:08 +0800

    圓餅圖顏色

--- 期末程式作業.py
+++ 期末程式作業.py
@@ -96,7 +96,6 @@ def open_index():  # 開啟首頁
     # 創建收入金額顯示 Label
     label_cost_num = Tk.Label(frame_labels_income, text=total_amount)
     label_cost_num.pack(side=Tk.TOP)
-
     #本月盈餘金額計算
     # 創建一個 Frame 作為框起來的區域
     frame_labels_surplus = Tk.Frame(index, bd=2, relief=Tk.GROOVE)
@@ -225,7 +224,7 @@ def open_cost():  # 開啟支出介面
         # 設置圓餅圖資料
         labels = items  # 各部分的標籤
         sizes = amounts  # 各部分的大小（百分比）
-        colors = ['gold', 'yellowgreen', 'lightcoral', 'lightskyblue']  # 顏色
+        colors = ['#' + ''.join(random.choices('0123456789ABCDEF', k=6)) for _ in range(len(labels))]  # 顏色
         explode = tuple(0.1 if i == max(amounts) else 0 for i in amounts)
         fig, ax = plt.subplots(figsize=(6, 6))
         ax.pie(sizes, explode=explode, labels=labels, colors=colors, autopct='%1.1f%%', shadow=True, startangle=140)
